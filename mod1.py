46c4cadefb44ce91c4308818d716239bcdda89ed
Sasha D. Hafner <sasha.hafner@gmail.com>
2022-12-04 20:49:52 -0500

    Start to turn into model function

--- mod1.py
+++ mod1.py
@@ -4,46 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
-import pdb
-
-# Set inputs
-L = 3     # Total length/height
-nc = 10  # Number of cells
-tt = 600 # Time (s)
-dt = 1    # Step (s)
-
-# Filter properties
-por = 0.5 # Gas phase porosity (m3/m3)
-cc = 0.0    # Initial concentration (g/m3)
-
-# Transport parameters
-#D = 1E-6  # m2/s
-Q = 0.001   # m3/s
-
-# Reaction 
-k = 1.E-5  # First-order 1/s
-
-# Dirty air chemical concentration
-cin = 0.1 # g/m3
-
-# Make sure cc is numeric to avoid integer crap
-cc = float(cc)
-
-# Number of time steps 
-nts = tt // dt 
-
-# Create cells
-x = np.linspace(0, L, nc + 1)  # Position in m
-dx = np.diff(x)
-x = x[1:(nc + 1)] - dx / 2
-
-# Cell gas volume (m3/m2)
-gv = dx * 1 * por 
-
-# Compound conc and mass (g)
-# cc = concentration, mc = mass, dims -> rows are cells (position), columns have time [position, time]
-cc = np.full((nc), cc)
-mc = cc * gv
+#import pdb
 
 # Rates function
 # Arg order: time, state variable, then arguments
@@ -54,18 +15,45 @@ def rates(t, mc, Q, cin, gv, k):
     # First cell derivative
     dm[0] = Q * (cin - cc[0]) - k * cc[0] 
     # Others
-    dm[1:nc] = Q * - np.diff(cc) - k * cc[1:nc]
+    dm[1:nc] = Q * -np.diff(cc) - k * cc[1:nc]
     
     return(dm)
 
-# Solve/integrate
-out = solve_ivp(rates, [0, 86400], y0 = mc, t_eval = np.linspace(0, 24, 24) * 3600, 
-        args = (Q, cin, gv, k))
+# Model function
+def tfmod(L, por, Q, nc, c0, cin, k):
 
-# Extract mass of compound [position, time]
-mct = out.y
-cct = mct / np.transpose(np.tile(gv, (mct.shape[1], 1)))
+    # L = total length/height (m)
+    # por = gas phase porosity (m3/m3)
+    # Q = gas flow rate (m3/s)
+    # nc = number of cells (layers)
+    # c0 = initial compound concentration (g/m3)
+    # cin = compound concentration in inflow (g/m3)
+    # k = first-order reaction rate (1/s)
 
-plt.plot(x, cct[:, 1]);plt.show()
-plt.plot(x, cct[:, 23]);plt.show()
+    # Make sure cc is numeric to avoid integer crap
+    c0 = float(c0)
+    
+    # Create cells
+    x = np.linspace(0, L, nc + 1)  
+    dx = np.diff(x)                # dx
+    x = x[1:(nc + 1)] - dx / 2     # Center position in m
+    
+    # Cell gas volume (m3/m2)
+    gv = dx * 1 * por 
+    
+    # Compound conc and mass (g)
+    # cc = concentration, mc = mass, dims -> rows are cells (position), columns have time [position, time]
+    cc = np.full((nc), c0)
+    mc = cc * gv
+    
+    # Solve/integrate
+    out = solve_ivp(rates, [0, 86400], y0 = mc, t_eval = np.linspace(0, 24, 24) * 3600, 
+            args = (Q, cin, gv, k))
+    
+    # Extract mass of compound [position, time]
+    mct = out.y
+    cct = mct / np.transpose(np.tile(gv, (mct.shape[1], 1)))
+    
+    plt.plot(x, cct[:, 1]);plt.show()
+    plt.plot(x, cct[:, 23]);plt.show()
 

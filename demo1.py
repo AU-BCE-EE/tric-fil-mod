fe8ce4230c911e7bcb39ccf9dcbf8cd593225c8a
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-01-03 15:10:13 -0500

    Add liq phase, needs fixes.
    Behavior not right. Also sorted out debugging with pdb.

--- demo1.py
+++ demo1.py
@@ -1,32 +1,40 @@
 # Demo of simple air filter model
 
+import pdb
 import numpy as np
 import matplotlib.pyplot as plt
 
-from mod1 import tfmod
-    
 # Set model inputs
 L = 2
-por = 0.5        # Gas-phase porosity (m3/m3)
+gas = 0.5        #  (m3/m3)
+liq = 0.25       #  (m3/m3)
 Q = 0.01         # Air flow (m3/s)
 nc = 10          # Number of model cells (layers)
-c0 = 0           # Initial compound concentration (g/m3)
-cin = 1          # Dirty air compound concentration (g/m3)
-k = 0.5 / 3600   # First-order degradation/removal rate (1/h -> 1/s)
+cg0 = 0          # (g/m3)
+cl0 = 0          # (g/m3)
+cgin = 2         # Dirty air compound concentration (g/L)
+ka = 1E-3        # (m3/s)
+k = 0.1 / 3600   # First-order degradation/removal rate (1/h -> 1/s)
+henry = (1000., 8000.)
+temp = 15
+dens = 1000
 
 # Times for model run
-tt = 0.2 # hours
+tt = 200 # hours
 times = np.linspace(0, tt, 500) * 3600
 
-# From function def: tfmod(L, por, Q, nc, c0, cin, k, times)
-pred = tfmod(L, por, Q, nc, c0, cin, k, times)
+from mod1 import tfmod
+pred = tfmod(L, gas, liq, Q, nc, cg0, cl0, cgin, ka, k, henry, temp, dens, times)
 
 ## Plot concentration within reactor
 #plt.plot(pred[2], pred[0][:, 0])
 #plt.plot(pred[2], pred[0][:, 1])
 
 # Plot outlet concentration (= 1 - removal efficiency here)
-plt.plot(pred[3] / 3600, pred[0][nc - 1, :])
-plt.savefig('outlet_conc.png')
+plt.plot(pred[5] / 3600, pred[0][nc - 1, :])
+plt.savefig('outlet_gas_conc.png')
+
+plt.plot(pred[5] / 3600, pred[1][nc - 1, :])
+plt.savefig('outlet_liq_conc.png')
 
 
--- mod1.py
+++ mod1.py
@@ -2,37 +2,82 @@
 
 # Packages
 import numpy as np
-import pdb
+import math
 from scipy.integrate import solve_ivp
 
 # Rates function
 # Arg order: time, state variable, then arguments
-def rates(t, mc, Q, cin, gv, k):
+def rates(t, mc, Q, cgin, vg, vl, k, ka, hc):
+
+    #breakpoint()
     
-    #pdb.set_trace()
-    cc = mc / gv
-    nc = cc.shape[0]
-    dm = np.zeros(cc.shape[0])
-    # First cell derivative
-    dm[0] = Q * (cin - cc[0]) - k * mc[0] 
-    # Other cell derivatives
-    dm[1:nc] = Q * -np.diff(cc) - k * mc[1:nc] # Check nc or nc - 1
-    
+    # Number of cells (layers) (note integer division)
+    nc = mc.shape[0] // 2
+
+    # Separate gas and liquid state variables
+    # Note that moronic Python language starts at 0 and ends at position beyond end of array
+    mcg = mc[0:nc]
+    mcl = mc[nc:(2 * nc)]
+
+    # Concentrations (g/L)
+    ccg = mcg / vg
+    ccl = mcl / vl
+
+    # Derivatives
+    # Set up empty arrays
+    dmg = dml = g2l = np.zeros(nc)
+
+    # Common term, mt into liquid phase (g/s)
+    g2l = ka * (ccg - ccl / hc) 
+
+    # Gas phase derivatives (g/s)
+    # cdiff = concentration difference (g/L)
+    cdiff = np.diff(np.insert(ccg, 0, cgin))
+    #     gas flow in    mt
+    dmg = Q * - cdiff - g2l
+
+    # Liquid phase derivatives (g/s)
+    #     mt  -reaction-
+    dml = g2l - k * mcl
+
+    # Combine gas and liquid
+    dm = np.concatenate([dmg, dml])
+
+    #breakpoint()
     return dm
 
 # Model function
-def tfmod(L, por, Q, nc, c0, cin, k, times):
+def tfmod(L, gas, liq, Q, nc, cg0, cl0, cgin, ka, k, henry, temp, dens, times):
 
     # L = total length/height (m)
-    # por = gas phase porosity (m3/m3)
+    # gas = gas phase porosity (m3/m3)
+    # liq = liquid phase content (m3/m3)
     # Q = gas flow rate (m3/s)
     # nc = number of cells (layers)
-    # c0 = initial compound concentration (g/m3)
-    # cin = compound concentration in inflow (g/m3)
-    # k = first-order reaction rate (1/s)
+    # cg0 = initial compound concentration in gas phase (g/m3)
+    # cl0 = initial compound concentration in liquid phase (g/m3)
+    # cgin = compound concentration in inflow (g/m3)
+    # ka = mass transfer coefficient for gas -> liquid (g/s / (g/m3) -> m3/s)
+    # k = first-order liquid phase reaction rate constant (1/s)
+    # henry = Henry's law constant coefficients as [k_H at 25 C, d(ln(kH)) / d(1/T)] as in NIST web book
+    # temp = temperature (degrees C)
+    # dens = solution (liquid) density (kg/m3)
 
-    # Make sure cc is numeric to avoid integer math bug
-    c0 = float(c0)
+    R = 0.083144 # L bar / K-mol
+    
+    # Make sure some inputs are numeric to avoid integer math bug
+    cg0 = float(cg0)
+    cl0 = float(cl0)
+    cgin = float(cgin)
+    ka = float(ka)
+    k = float(k)
+    temp = float(temp)
+
+    # Temperature and Henry's law constant
+    TK = temp + 273.15
+    kh = henry[0] * math.exp(henry[1] * (1/TK - 1/298.15)) # mol/kg-bar as liq:gas
+    kh = kh * dens # mol/L-bar
+    hc = kh * R * TK # dimensionless, liq:gas, e.g., g/L / g/L
     
     # Create cells
     x = np.linspace(0, L, nc + 1)  # Original nc + 1 values
@@ -40,22 +85,34 @@ def tfmod(L, por, Q, nc, c0, cin, k, times):
     x = x[1:(nc + 1)] - dx / 2     # Center position in m
     
     # Cell gas volume (m3/m2)
-    gv = dx * 1 * por 
+    vg = dx * 1 * gas 
+    # Cell liquid volume (m3/m2)
+    vl = dx * 1 * liq 
     
     # Compound conc and mass (g)
     # cc = concentration, mc = mass [position]
-    cc = np.full((nc), c0)
-    mc = cc * gv
+    # g = gas, l = liquid
+    ccg = np.full((nc), cg0)
+    ccl = np.full((nc), cl0)
+    mcg = ccg * vg
+    mcl = ccl * vl
 
+    # Initial state variable array (Python language is terrible)
+    y0 = np.concatenate([mcg, mcl])
     
     # Solve/integrate
-    out = solve_ivp(rates, [0, max(times)], y0 = mc, 
+    out = solve_ivp(rates, [0, max(times)], y0 = y0, 
                     t_eval = times, 
-                    args = (Q, cin, gv, k))
+                    args = (Q, cgin, vg, vl, k, ka, hc))
     
     # Extract mass of compound [position, time]
-    mct = out.y
-    cct = mct / np.transpose(np.tile(gv, (mct.shape[1], 1)))
+    mcgt = out.y[0:nc]
+    mclt = out.y[nc:(2 * nc)]
+    # Get concentrations
+    ccgt = mcgt / np.transpose(np.tile(vg, (mcgt.shape[1], 1)))
+    cclt = mclt / np.transpose(np.tile(vl, (mclt.shape[1], 1)))
+
+    mct = np.concatenate([mcgt, mclt])
     
     # Return results as a tuple
-    return cct, mct, x, times
+    return ccgt, cclt, mcgt, mclt, x, times

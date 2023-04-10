869552a950c9909cc2be0e180b4f3a21cf70160d
Sasha D. Hafner <sasha@hafnerconsulting.com>
2023-04-10 11:07:16 -0400

    Add countercurrent flow

---
+++ demos/06_counter_current/demo.py
@@ -0,0 +1,99 @@
+# Demo of Python air filter model
+
+# Import necessary packages ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import model ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+shutil.copy('../../tfmod.py', '.')
+from tfmod import tfmod
+
+# Set model inputs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# See notes in tfmod.py for more complete descriptions
+L = 2            # Filter length/depth (m)
+por_g = 0.5      # (m3/m3)
+por_l = 0.25     # (m3/m3)
+v_g = 0.03       # Air flow (m/s)
+v_l = 2E-5       # Water flow (m/s)
+nc = 30          # Number of model cells (layers)
+cg0 = 0          # (g/m3)
+cl0 = 0          # (g/m3)
+cgin = 1.        # Dirty air compound concentration (g/m3)
+clin = 0.        # Fresh water concentration (g/m3)
+henry = (0.1, 2000.)
+temp = 15.       # (degrees C)
+dens = 1000      # Liquid density (kg/m3)
+
+Kga = 1.         # Mass transfer coefficient (1/s)
+k = 10. / 3600
+
+pH = 7.
+pKa = 7.
+
+
+# Times for model output, calculated from tt and nt here but could be set directly
+# Total duration (hours)
+tt = 2 
+# Number of time rows
+nt = 500
+times = np.linspace(0, tt, nt) * 3600
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Scenarios ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Sim 1 pH = pKa
+pred1 = tfmod(L = L, por_g = por_g, por_l = por_l, v_g = v_g, v_l = v_l, nc = nc, cg0 = cg0, 
+              cl0 = cl0, cgin = cgin, clin = clin, Kga = Kga, k = k, henry = henry, pKa = pKa, 
+              pH = pH, temp = temp, dens = dens, times = times)
+
+# Sim 2 countercurrent flow
+pred2 = tfmod(L = L, por_g = por_g, por_l = por_l, v_g = v_g, v_l = v_l, nc = nc, cg0 = cg0, 
+              cl0 = cl0, cgin = cgin, clin = clin, Kga = Kga, k = k, henry = henry, pKa = pKa, 
+              pH = pH, temp = temp, dens = dens, times = times, counter = True)
+
+# Sim 3 countercurrent with higher and dirty liquid flow and clean air
+
+clin = 3.
+cgin = 0.
+v_l = 0.01
+pred3 = tfmod(L = L, por_g = por_g, por_l = por_l, v_g = v_g, v_l = v_l, nc = nc, cg0 = cg0, 
+              cl0 = cl0, cgin = cgin, clin = clin, Kga = Kga, k = k, henry = henry, pKa = pKa, 
+              pH = pH, temp = temp, dens = dens, times = times, counter = True)
+
+# Plots ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Plot outlet concentration (= 1 - removal efficiency here because cgin = 1)
+# Gas concentration (outlet air) 
+plt.plot(pred1[5] / 3600, pred1[0][nc - 1, :], 'r-')
+plt.plot(pred2[5] / 3600, pred2[0][nc - 1, :], 'b', linestyle = 'dashed')
+plt.plot(pred3[5] / 3600, pred3[0][nc - 1, :], 'g-')
+plt.xlabel('Time (h)')
+plt.ylabel('Compound conc. (g/m3)')
+plt.savefig('outlet_gas_conc.png')
+
+# Liquid concentration (in last layer)
+plt.clf()
+plt.plot(pred1[5] / 3600, pred1[1][nc - 1, :], 'r-')
+plt.plot(pred2[5] / 3600, pred2[1][nc - 1, :], 'b', linestyle = 'dashed')
+plt.plot(pred3[5] / 3600, pred3[1][nc - 1, :], 'g-')
+plt.xlabel('Time (h)')
+plt.ylabel('Compound conc. (g/m3)')
+plt.savefig('outlet_liq_conc.png')
+
+# Profiles
+# Gas
+plt.clf()
+plt.plot(pred1[4], pred1[0][:, nt - 1], 'r-')
+plt.plot(pred2[4], pred2[0][:, nt - 1], 'b', linestyle = 'dashed')
+plt.plot(pred3[4], pred3[0][:, nt - 1], 'g-')
+plt.xlabel('Location (m)')
+plt.ylabel('Compound conc. (g/m3)')
+plt.savefig('profile_gas_conc.png')
+
+# Liquid
+plt.clf()
+plt.plot(pred1[4], pred1[1][:, nt - 1], 'r-')
+plt.plot(pred2[4], pred2[1][:, nt - 1], 'b', linestyle = 'dashed')
+plt.plot(pred3[4], pred3[1][:, nt - 1], 'g-')
+plt.xlabel('Location (m)')
+plt.ylabel('Compound conc. (g/m3)')
+plt.savefig('profile_liq_conc.png')

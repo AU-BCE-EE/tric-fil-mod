fda4e1fb830d4d41163a2f19030298a2fdab3506
AnneMortensen <142310434+AnneMortensen@users.noreply.github.com>
2024-03-04 14:52:23 +0100

    Reorganization of data treatment
    Tried to make experiment 2 separate from the other experiments, so that the folders "Raw data", "Plots" and so on doesn't get too crowded

--- applications/Modelling reservoir TEST/Reservoir modelling demo.py
+++ applications/Modelling reservoir TEST/Reservoir modelling demo.py
@@ -8,7 +8,8 @@ import pandas as pd
 
 # Import model ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-from Reservoir_modelling_mod_funcs import tfmod  
+shutil.copy('../../mod_funcs.py', '.')
+from mod_funcs import tfmod  
 
 
 # Set model inputs ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -36,7 +37,7 @@ pKa = 7.
 
 
 # Put inlet concentrations at equilibrium
-cgin = 0.02  #corresponding to 15ppm
+cgin = 0.055  #corresponding to 15ppm
 clin = 0
 
 # Times for model output in h
@@ -51,28 +52,28 @@ times = np.linspace(0, tt, nt) * 3600
 #v_g=60m/h, v_l=0.4m/h
 pred1 = tfmod(L = L, por_g = por_g, por_l = por_l, v_g = 60/3600, v_l = 0.4/3600, nc = nc, cg0 = cg0, 
               cl0 = cl0, cgin = cgin, clin = clin, Kga = 'onda', k = k, henry = henry, pKa = pKa, 
-              pH = pH, temp = temp, dens_l = dens_l, times = times, nt=nt, tt=tt, v_res =0)
+              pH = pH, temp = temp, dens_l = dens_l, times = times, recirc = True)
 pred1label = 'v_res = 0'
 
 # Blue
 #v_g=60m/h, v_l=1m/h
 pred2 = tfmod(L = L, por_g = por_g, por_l = por_l, v_g = 60/3600, v_l = 0.4/3600, nc = nc, cg0 = cg0, 
               cl0 = cl0, cgin = cgin, clin = clin, Kga = 'onda', k = k, henry = henry, pKa = pKa, 
-              pH = pH, temp = temp, dens_l = dens_l, times = times ,nt=nt, tt=tt, v_res = 0.0018)
+              pH = pH, temp = temp, dens_l = dens_l, times = times, v_res = 0.0018, recirc = True)
 pred2label = 'v_res = 0.0018m3'
 
 # Green line in plots
 #v_g=100m/h, v_l=0.4m/h
 pred3 = tfmod(L = L, por_g = por_g, por_l = por_l,v_g = 60/3600, v_l = 0.4/3600, nc = nc, cg0 = cg0, 
               cl0 = cl0, cgin = cgin, clin = clin, Kga = 'onda', k = k, henry = henry, pKa = pKa, 
-              pH = pH, temp = temp, dens_l = dens_l, times = times, nt = nt, tt = tt, v_res = 0.01)
+              pH = pH, temp = temp, dens_l = dens_l, times = times, v_res = 0.01, recirc = True)
 pred3label = 'v_res = 0.01m3'
 
 # Orange line in plots
 #v_g=100m/h, v_l=1m/h
 pred4 = tfmod(L = L, por_g = por_g, por_l = por_l,v_g = 60/3600, v_l = 0.4/3600, nc = nc, cg0 = cg0, 
               cl0 = cl0, cgin = cgin, clin = clin, Kga = 'onda', k = k, henry = henry, pKa = pKa, 
-              pH = pH, temp = temp, dens_l = dens_l, times = times, nt = nt, tt = tt, v_res = 0.1)
+              pH = pH, temp = temp, dens_l = dens_l, times = times, v_res = 0.1, recirc = True)
 pred4label = 'v_res = 0.1m3'
 
 
@@ -119,7 +120,7 @@ plt.show()
 #Liquid
 plt.plot(pred1['time'] / 3600, pred1['liq_conc'][nc - 1, :], 'r',label=pred1label)
 plt.plot(pred2['time'] / 3600, pred2['liq_conc'][nc - 1, :], 'r',linestyle='dashed',label=pred2label)
-plt.plot(pred3['time'] / 3600, pred3['liq_conc'][nc - 1, :], 'b',label='v_g=100m/h, v_l=0.4m/h')
+plt.plot(pred3['time'] / 3600, pred3['liq_conc'][nc - 1, :], 'b',label=pred3label)
 plt.plot(pred4['time'] / 3600, pred4['liq_conc'][nc - 1, :], 'b',linestyle='dashed',label=pred4label)
 plt.xlabel('Time (h)')
 plt.ylabel('Compound conc. (g/m3)')
